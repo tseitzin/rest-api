9e070641a9cda0486c0e502387c5c2c519fb2b3b
Tim <tseitzinger@gmail.com>
2018-01-31 17:23:41 -0700

    Initial commit for Heroku

---
+++ resources/user.py
@@ -0,0 +1,19 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+    
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help='This field can not be left blank')
+    parser.add_argument('password', type=str, required=True, help='This field can not be left blank')
+ 
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        
+        if UserModel.find_by_username(data['username']):
+            return {'message': 'This username is already in use'}, 400
+
+        user = UserModel(data['username'], data['password'])     
+        user.save_to_db()   
+        return {'message': 'User was successfully created'}, 201
+           
